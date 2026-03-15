c43ba790ee027e674cb1c83f498bd0351d9957f5
montoyacf-png <montoyacf@gmail.com>
2026-03-15 12:13:31 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,22 @@
-
+const CACHE_NAME = 'cnsc-v802';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('cnsc-v801').then(cache => cache.addAll(['./', 'index.html', 'manifest.json'])));
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./','index.html','manifest.json'])));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+      return res;
+    }).catch(() => caches.match(req))
+  );
 });
